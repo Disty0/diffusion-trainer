abba8e4cfb2096adf7071ead4b44aefb9e224cb0
Disty0 <disty@disty.xyz>
2025-07-19 15:31:44 +0300

    Add AdaMuon

--- utils/optimizers/muon.py
+++ utils/optimizers/muon.py
@@ -3,6 +3,152 @@ from .stochastic import copy_stochastic_
 
 from utils.sdnq_utils import int8_matmul_dynamic, fp8_matmul_dynamic
 
+
+class MuonWithAuxAdam(torch.optim.Optimizer):
+    """
+    Non-distributed variant of MuonWithAuxAdam.
+    """
+    def __init__(self, param_groups):
+        for group in param_groups:
+            assert "use_muon" in group
+            if group["use_muon"]:
+                # defaults
+                group["lr"] = group.get("lr", 0.02)
+                group["betas"] = group.get("betas", (0.95, 0.999))
+                group["eps"] = group.get("eps", 1e-8)
+                group["weight_decay"] = group.get("weight_decay", 0)
+                group["ns_steps"] = group.get("ns_steps", 5)
+                group["nesterov"] = group.get("nesterov", False)
+                group["adaptive"] = group.get("adaptive", False)
+                group["bf16_stochastic_round"] = group.get("bf16_stochastic_round", False)
+                group["zeropower_dtype"] = group.get("zeropower_dtype", "bfloat16")
+                group["use_quantized_matmul"] = group.get("use_quantized_matmul", False)
+                group["quantized_matmul_dtype"] = group.get("quantized_matmul_dtype", "int8")
+                if isinstance(group["zeropower_dtype"], str):
+                    group["zeropower_dtype"] = getattr(torch, group["zeropower_dtype"])
+                assert set(group.keys()) == set(["params", "lr", "betas", "eps", "weight_decay", "ns_steps", "adaptive", "nesterov", "bf16_stochastic_round", "use_muon", "zeropower_dtype", "use_quantized_matmul", "quantized_matmul_dtype"])
+            else:
+                # defaults
+                group["lr"] = group.get("lr", 3e-4)
+                group["betas"] = group.get("betas", (0.9, 0.95))
+                group["eps"] = group.get("eps", 1e-10)
+                group["weight_decay"] = group.get("weight_decay", 0)
+                group["bf16_stochastic_round"] = group.get("bf16_stochastic_round", False)
+                assert set(group.keys()) == set(["params", "lr", "betas", "eps", "weight_decay", "bf16_stochastic_round", "use_muon"])
+        super().__init__(param_groups, dict())
+
+    @torch.no_grad()
+    def step(self, closure=None):
+
+        loss = None
+        if closure is not None:
+            with torch.enable_grad():
+                loss = closure()
+
+        for group in self.param_groups:
+            if group["use_muon"]:
+                for p in group["params"]:
+                    if p.grad is None:
+                        continue
+                    state = self.state[p]
+                    if len(state) == 0:
+                        state["momentum_buffer"] = torch.zeros_like(p)
+                        if group["adaptive"]:
+                            state["v_buffer"] = torch.zeros_like(p)
+                            state["step"] = 0
+                    update = muon_update(
+                        p.grad,
+                        state,
+                        betas=group["betas"],
+                        eps=group["eps"],
+                        ns_steps=group["ns_steps"],
+                        nesterov=group["nesterov"],
+                        adaptive=group["adaptive"],
+                        zeropower_dtype=group["zeropower_dtype"],
+                        use_quantized_matmul=group["use_quantized_matmul"],
+                        quantized_matmul_dtype=group["quantized_matmul_dtype"],
+                    )
+                    if group["adaptive"]:
+                        alpha = -group["lr"] * 0.2 * max(update.size(-2), update.size(-1))**0.5
+                    else:
+                        alpha = -group["lr"] * max(1, update.size(-2) / update.size(-1))**0.5
+                    if group["bf16_stochastic_round"]:
+                        p_fp32 = p.to(torch.float32)
+                        if group["weight_decay"] > 0:
+                            p_fp32.mul_(1 - group["lr"] * group["weight_decay"])
+                        p_fp32.add_(update.view(p.shape), alpha=alpha)
+                        copy_stochastic_(p, p_fp32)
+                    else:
+                        if group["weight_decay"] > 0:
+                            p.mul_(1 - group["lr"] * group["weight_decay"])
+                        p.add_(update.view(p.shape), alpha=alpha)
+            else:
+                for p in group["params"]:
+                    if p.grad is None:
+                        continue
+                    state = self.state[p]
+                    if len(state) == 0:
+                        state["exp_avg"] = torch.zeros_like(p)
+                        state["exp_avg_sq"] = torch.zeros_like(p)
+                        state["step"] = 0
+                    state["step"] += 1
+                    update = adam_update(
+                        p.grad,
+                        state["exp_avg"],
+                        state["exp_avg_sq"],
+                        state["step"],
+                        group["betas"],
+                        group["eps"]
+                    )
+                    if group["bf16_stochastic_round"]:
+                        p_fp32 = p.to(torch.float32)
+                        if group["weight_decay"] > 0:
+                            p_fp32.mul_(1 - group["lr"] * group["weight_decay"])
+                        p_fp32.add_(update, alpha=-group["lr"])
+                        copy_stochastic_(p, p_fp32)
+                    else:
+                        if group["weight_decay"] > 0:
+                            p.mul_(1 - group["lr"] * group["weight_decay"])
+                        p.add_(update, alpha=-group["lr"])
+
+        return loss
+
+
+def muon_update(grad, state, betas=(0.95, 0.999), eps=1e-8, ns_steps=5, nesterov=True, adaptive=False, zeropower_dtype=torch.bfloat16, use_quantized_matmul=False, quantized_matmul_dtype="int8"):
+    beta, beta2 = betas
+    momentum = state["momentum_buffer"]
+    momentum.lerp_(grad, 1 - beta)
+    grad = grad.lerp_(momentum, beta) if nesterov else momentum
+    if grad.ndim == 4: # for the case of conv filters
+        grad = grad.view(len(grad), -1)
+    if use_quantized_matmul:
+        if quantized_matmul_dtype == "int8":
+            grad = zeropower_via_newtonschulz5_int8_matmul(grad, steps=ns_steps, dtype=zeropower_dtype)
+        elif quantized_matmul_dtype == "fp8":
+            grad = zeropower_via_newtonschulz5_fp8_matmul(grad, steps=ns_steps, dtype=zeropower_dtype)
+        else:
+            raise NotImplementedError(f'Quantization type {quantized_matmul_dtype} is not implemented')
+    else:
+        grad = zeropower_via_newtonschulz5(grad, steps=ns_steps, dtype=zeropower_dtype)
+    if adaptive:
+        v = state["v_buffer"]
+        state["step"] += 1
+        v.mul_(beta2).addcmul_(grad, grad, value=(1 - beta2))
+        v_hat = v / (1 - beta2 ** state["step"])
+        grad.div_(v_hat.view_as(grad).sqrt_().add_(eps))
+        grad.mul_(min(grad.shape)**0.5 / (grad.norm().add_(eps)))
+    return grad
+
+
+def adam_update(grad, buf1, buf2, step, betas, eps):
+    beta, beta2 = betas
+    buf1.lerp_(grad, 1 - beta)
+    buf2.lerp_(grad.square(), 1 - beta2)
+    buf1c = buf1 / (1 - beta ** step)
+    buf2c = buf2 / (1 - beta2 ** step)
+    return buf1c.div_(buf2c.sqrt_().add_(eps))
+
+
 def zeropower_via_newtonschulz5(G, steps: int, dtype=torch.bfloat16):
     """
     Newton-Schulz iteration to compute the zeroth power / orthogonalization of G. We opt to use a
@@ -72,113 +218,6 @@ def zeropower_via_newtonschulz5_fp8_matmul(G, steps: int, dtype=torch.bfloat16):
     if G.size(-2) > G.size(-1):
         X = X.mT
     return X
-
-
-def muon_update(grad, momentum, beta=0.95, ns_steps=5, nesterov=True, zeropower_dtype=torch.bfloat16, use_quantized_matmul=False, quantized_matmul_dtype="int8"):
-    momentum.lerp_(grad, 1 - beta)
-    update = grad.lerp_(momentum, beta) if nesterov else momentum
-    if update.ndim == 4: # for the case of conv filters
-        update = update.view(len(update), -1)
-    if use_quantized_matmul:
-        if quantized_matmul_dtype == "int8":
-            update = zeropower_via_newtonschulz5_int8_matmul(update, steps=ns_steps, dtype=zeropower_dtype)
-        elif quantized_matmul_dtype == "fp8":
-            update = zeropower_via_newtonschulz5_fp8_matmul(update, steps=ns_steps, dtype=zeropower_dtype)
-        else:
-            raise NotImplementedError(f'Quantization type {quantized_matmul_dtype} is not implemented')
-    else:
-        update = zeropower_via_newtonschulz5(update, steps=ns_steps, dtype=zeropower_dtype)
-    update *= max(1, grad.size(-2) / grad.size(-1))**0.5
-    return update
-
-
-def adam_update(grad, buf1, buf2, step, betas, eps):
-    buf1.lerp_(grad, 1 - betas[0])
-    buf2.lerp_(grad.square(), 1 - betas[1])
-    buf1c = buf1 / (1 - betas[0]**step)
-    buf2c = buf2 / (1 - betas[1]**step)
-    return buf1c / (buf2c.sqrt() + eps)
-
-
-class MuonWithAuxAdam(torch.optim.Optimizer):
-    """
-    Non-distributed variant of MuonWithAuxAdam.
-    """
-    def __init__(self, param_groups):
-        for group in param_groups:
-            assert "use_muon" in group
-            if group["use_muon"]:
-                # defaults
-                group["lr"] = group.get("lr", 0.02)
-                group["momentum"] = group.get("momentum", 0.95)
-                group["weight_decay"] = group.get("weight_decay", 0)
-                group["bf16_stochastic_round"] = group.get("bf16_stochastic_round", False)
-                group["zeropower_dtype"] = group.get("zeropower_dtype", "bfloat16")
-                group["use_quantized_matmul"] = group.get("use_quantized_matmul", False)
-                group["quantized_matmul_dtype"] = group.get("quantized_matmul_dtype", "int8")
-                if isinstance(group["zeropower_dtype"], str):
-                    group["zeropower_dtype"] = getattr(torch, group["zeropower_dtype"])
-                assert set(group.keys()) == set(["params", "lr", "momentum", "weight_decay", "bf16_stochastic_round", "use_muon", "zeropower_dtype", "use_quantized_matmul", "quantized_matmul_dtype"])
-            else:
-                # defaults
-                group["lr"] = group.get("lr", 3e-4)
-                group["betas"] = group.get("betas", (0.9, 0.95))
-                group["eps"] = group.get("eps", 1e-10)
-                group["weight_decay"] = group.get("weight_decay", 0)
-                group["bf16_stochastic_round"] = group.get("bf16_stochastic_round", False)
-                assert set(group.keys()) == set(["params", "lr", "betas", "eps", "weight_decay", "bf16_stochastic_round", "use_muon"])
-        super().__init__(param_groups, dict())
-
-    @torch.no_grad()
-    def step(self, closure=None):
-
-        loss = None
-        if closure is not None:
-            with torch.enable_grad():
-                loss = closure()
-
-        for group in self.param_groups:
-            if group["use_muon"]:
-                for p in group["params"]:
-                    if p.grad is None:
-                        continue
-                    state = self.state[p]
-                    if len(state) == 0:
-                        state["momentum_buffer"] = torch.zeros_like(p)
-                    update = muon_update(p.grad, state["momentum_buffer"], beta=group["momentum"], zeropower_dtype=group["zeropower_dtype"], use_quantized_matmul=group["use_quantized_matmul"], quantized_matmul_dtype=group["quantized_matmul_dtype"])
-                    if group["bf16_stochastic_round"]:
-                        p_fp32 = p.to(torch.float32)
-                        if group["weight_decay"] > 0:
-                            p_fp32.mul_(1 - group["lr"] * group["weight_decay"])
-                        p_fp32.add_(update.reshape(p.shape), alpha=-group["lr"])
-                        copy_stochastic_(p, p_fp32)
-                    else:
-                        if group["weight_decay"] > 0:
-                            p.mul_(1 - group["lr"] * group["weight_decay"])
-                        p.add_(update.reshape(p.shape), alpha=-group["lr"])
-            else:
-                for p in group["params"]:
-                    if p.grad is None:
-                        continue
-                    state = self.state[p]
-                    if len(state) == 0:
-                        state["exp_avg"] = torch.zeros_like(p)
-                        state["exp_avg_sq"] = torch.zeros_like(p)
-                        state["step"] = 0
-                    state["step"] += 1
-                    update = adam_update(p.grad, state["exp_avg"], state["exp_avg_sq"], state["step"], group["betas"], group["eps"])
-                    if group["bf16_stochastic_round"]:
-                        p_fp32 = p.to(torch.float32)
-                        if group["weight_decay"] > 0:
-                            p_fp32.mul_(1 - group["lr"] * group["weight_decay"])
-                        p_fp32.add_(update, alpha=-group["lr"])
-                        copy_stochastic_(p, p_fp32)
-                    else:
-                        if group["weight_decay"] > 0:
-                            p.mul_(1 - group["lr"] * group["weight_decay"])
-                        p.add_(update, alpha=-group["lr"])
-
-        return loss
 
 
 torch._dynamo.config.cache_size_limit = max(8192, torch._dynamo.config.cache_size_limit)
